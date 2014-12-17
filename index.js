a96f176c2522d882903b8be0a752a13f44c9c3c6
Mathieu Garcia <mgarcia@wiseflat.com>
2014-12-18 00:38:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var xpldomogeek = require("./lib/xpl-domogeek");
+
+var wt = new xpldomogeek(null, {
+	//xplSource: 'bnz-ipx800.wiseflat'
+});
+
+wt._init(function(error, xpl) {
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+        
+        wt.getLocalConfig();
+        
+        
+        
+        xpl.on("xpl:domogeek.config", function(evt) {
+		console.log("Receive message domogeek.config ", evt);
+                if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
+        }); 
+
+        xpl.on("xpl:domogeek.request", function(evt) {
+		console.log("Receive message domogeek.request ", evt);
+                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
+        });
+});
+
